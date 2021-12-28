92558dc19da7c1f0d944ba195f323e86fafdb0ff
nerdyjohnny <13726809+nerdyjohnny@users.noreply.github.com>
2021-12-28 16:36:05 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Mutuals Integrated Love Federation'
+copyright = '2021, nerdyjohnny and Oiivae'
+author = 'nerdyjohnny'
 
 release = '0.1'
 version = '0.1.0'
