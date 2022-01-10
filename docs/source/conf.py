792adc0c5b6c7eb8f35b300088af1ddec595cc0f
nerdyjohnny <13726809+nerdyjohnny@users.noreply.github.com>
2022-01-10 17:20:25 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'League of Virtual Entertainers'
 copyright = '2021, nerdyjohnny and Oiivae'
 author = 'nerdyjohnny'
 
-release = '1.0'
-version = '1.0.0'
+release = '0.5'
+version = '0.5.0'
 
 # -- General configuration
 
